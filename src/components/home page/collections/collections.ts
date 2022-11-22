f0f136623592660581e71cc5aef67e9c49590124
Erickson <ericksonrodrigues13@gmail.com>
2022-11-21 23:21:34 -0300

    collections section: responsiveness for mobile screens

--- src/components/home page/collections/collections.ts
+++ src/components/home page/collections/collections.ts
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 export const SectionHand = styled.section`
     background-color: #1B4B66;
     color: #FFFFFF;
+    @media (max-width:375px) {
+        width:113%
+    }
+
+    @media (max-width:320px) {
+        width:133%
+    }
 `
 
 export const Handpickedh1 = styled.h1`
@@ -15,7 +22,27 @@ export const DivCollections = styled.div`
     display: flex;
     justify-content: space-around;
     padding-bottom: 2rem;
+
+    @media (max-width:425px) {
+        flex-wrap: wrap;
+        margin-left: 1.5rem;
+    }
+
+    
 `
 
+export const Img_flex = styled.img`
+
+    @media (max-width:425px) {
+        width:90%;
+        margin-bottom:1rem;
+    }  
+`
+export const Div_flex = styled.div`
+    @media (max-width:425px) {
+        flex-grow: 1;
+        width: 50%;
+    }  
+`
 
 
--- src/components/home page/collections/index.tsx
+++ src/components/home page/collections/index.tsx
@@ -3,28 +3,37 @@ import Img_handbags from '../../../assets/Images/Home Page/handbags.png'
 import Img_watches from '../../../assets/Images/Home Page/watches.png'
 import Img_glasses from '../../../assets/Images/Home Page/glasses.png'
 
-import { SectionHand, Handpickedh1, DivCollections  } from './collections'
+import { SectionHand, Handpickedh1, DivCollections, Img_flex, Div_flex} from './collections'
 
 function Handpicked() {
    return (
       <SectionHand>
          <Handpickedh1>Handpicked Collections </Handpickedh1>
          <DivCollections >
-            <a href="./src/assets/Pages/page404.html">
-               <img src={Img_personalCare} alt="Personal care" />
-            </a>
-
-            <a href="./src/assets/Pages/page404.html">
-               <img src={Img_handbags} alt="Handbags" /> 
-            </a>
-            
-            <a href="./src/assets/Pages/page404.html">
-               <img src={Img_watches} alt="Wrist Watches" />
-            </a>
-            
-            <a href="./src/assets/Pages/page404.html">
-               <img src={Img_glasses} alt="Sun Glasses" />
-            </a>
+            <Div_flex>
+               <a href="./src/assets/Pages/page404.html">
+                  <Img_flex  src={Img_personalCare} alt="Personal care" />
+               </a>
+            </Div_flex>
+               
+            <Div_flex>
+               <a href="./src/assets/Pages/page404.html">
+                  <Img_flex  src={Img_handbags} alt="Handbags" /> 
+               </a>
+            </Div_flex>
+              
+            <Div_flex>
+               <a href="./src/assets/Pages/page404.html">
+                  <Img_flex  src={Img_watches} alt="Wrist Watches" />
+               </a>
+            </Div_flex>
+               
+            <Div_flex>
+               <a href="./src/assets/Pages/page404.html">
+                  <Img_flex  src={Img_glasses} alt="Sun Glasses" />
+               </a>
+            </Div_flex>
+               
             
          </DivCollections >
       </SectionHand>

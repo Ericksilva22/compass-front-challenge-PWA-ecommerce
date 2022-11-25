afe43c6789aeb80000b5608965a16a735c839f22
Erickson <ericksonrodrigues13@gmail.com>
2022-11-25 10:38:44 -0300

    Storybook: category page

---
+++ src/stories/handbags.stories.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+import MainHandbags from '../components/Categoty page/handbags/page 1/index'
+
+export default {
+    title: 'Category page/Handbags',
+    component: MainHandbags,
+}
+
+export const Default = {
+    
+}
---
+++ src/stories/watches.stories.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+import MainWatches from '../components/Categoty page/watches/page 1/index'
+
+export default {
+    title: 'Category page/Watches',
+    component: MainWatches,
+}
+
+export const Default = {
+    
+}

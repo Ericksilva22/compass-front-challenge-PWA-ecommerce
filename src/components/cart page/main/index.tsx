b64bbc6d76601cfb2609e54a5c83f99ae033b8bd
Erickson <ericksonrodrigues13@gmail.com>
2022-11-30 12:06:46 -0300

    My cart page: create product section

--- src/components/cart page/main/index.tsx
+++ src/components/cart page/main/index.tsx
@@ -1,21 +1,45 @@
 import arrow_nav from '../../../assets/Images/Product Page/chevron-right-small.svg'
 import arrow_nav_up from '../../../assets/Images/Checkout Page/Vector-up.svg'
 import arrow_nav_down from '../../../assets/Images/Checkout Page/Vector-down.svg'
-import Img_UPI from '../../../assets/Images/Checkout Page/UPI-Logo-vector 1.svg'
-import Img_credit from '../../../assets/Images/Checkout Page/Vector-card.svg'
-import Img_amazon from '../../../assets/Images/Checkout Page/image 30.svg'
-import Img_apple from '../../../assets/Images/Checkout Page/image 28.svg'
-import Img_Google from '../../../assets/Images/Checkout Page/Group 660.svg'
-import Img_Pe from '../../../assets/Images/Checkout Page/Group 9884.svg'
-import Img_Paytm from '../../../assets/Images/Checkout Page/Group 9885.svg'
+import Img_coach from '../../../assets/Images/Checkout Page/image-coach.svg'
 
 import { Div_options, Div_options_TitleGray, Div_options_Title, H1_main, Main, Div_order, P_order, Button_order,
-         Button_continue} from './main'
+         Button_continue, Section_cart, Div_cart, Div_name, Div_product, Div_product_descriptions, Link_primary,
+         Link_Sencondary, Div_Flex_Cart, Div_apply_cart} from './main'
 
-import {H1_order, Div_details, P_details} from '../../checkout page/Order/Order'
+import {H1_order, Div_details, P_details, Figure, H3_order} from '../../checkout page/Order/Order'
 
+import {Div_flex, H2_main, Img_none} from '../../checkout page/main/main'
+
+import {Div_apply, P_Div_apply, A_Div_apply} from '../../modal box/modal'
 
 function MainCart() {
+
+   function MoreCart(){
+
+      const icon_down = document.querySelector<HTMLElement>("#Icon_down_cart")!;
+      const icon_up = document.querySelector<HTMLElement>("#Icon_up_cart")!;
+
+      const Div_Apply = document.querySelector<HTMLElement>("#Div_apply")!;
+
+      icon_down.style.display = "block";
+      icon_up.style.display = "none";
+      Div_Apply.style.display = "flex";
+
+      
+   }
+
+   function CloseCart(){
+      const icon_down = document.querySelector<HTMLElement>("#Icon_down_cart")!;
+      const icon_up = document.querySelector<HTMLElement>("#Icon_up_cart")!;
+
+      const Div_Apply = document.querySelector<HTMLElement>("#Div_apply")!;
+
+      icon_down.style.display = "none";
+      icon_up.style.display = "block";
+      Div_Apply.style.display = "none";
+      
+   }
 
     return (
       <Main>
@@ -25,11 +49,56 @@ function MainCart() {
             <Div_options_TitleGray>My cart</Div_options_TitleGray>
          </Div_options>
          <H1_main>My cart</H1_main>
-         <section>
-            <div>
+         <Section_cart>
+            <Div_cart>
+               <Div_name>
+                  <p>Product Name</p>
+                  <div>
+                     <p>Price</p>
+                     <p>Qty</p>
+                     <p>Subtotal</p>
+                  </div>
+               </Div_name>
 
-            </div>
+               <Div_product>
+                  <Div_Flex_Cart >
+                     <Figure>
+                        <img src={Img_coach} alt="" />
+                     </Figure>
 
+                     <div>
+                        <H3_order>Coach</H3_order>
+                        <P_order >Leather Coach Bag</P_order >
+                        <P_order >Qty- 1</P_order >
+                     </div>
+                  </Div_Flex_Cart >
+
+                  <Div_product_descriptions>
+                     <div>
+                        <p>$54.69</p>
+                        <p>1</p>
+                        <p>54.69</p>
+                     </div>
+
+                     <div>
+                        <Link_primary href="">Move to Wishlist</Link_primary>
+                        <Link_Sencondary href="">Remove</Link_Sencondary>
+                     </div>
+                  </Div_product_descriptions>
+               </Div_product>
+
+               <Div_flex>
+                  <H2_main>Apply Coupon Code</H2_main>
+                  <img src={arrow_nav_down} id="Icon_down_cart" onClick={CloseCart} alt="" />
+                  <Img_none src={arrow_nav_up} id="Icon_up_cart" onClick={MoreCart} alt="" />
+               </Div_flex>
+
+               
+               <Div_apply_cart id='Div_apply'>
+                  <P_Div_apply>Apply Coupon Code</P_Div_apply>
+                  <A_Div_apply href="../page404.html">CHECK</A_Div_apply>
+               </Div_apply_cart>
+            </Div_cart>
             <Div_order>
                <H1_order>Order Summary</H1_order>
                <Div_details>
@@ -58,7 +127,7 @@ function MainCart() {
                </Div_details>
                
             </Div_order>
-         </section>
+         </Section_cart>
       </Main>
     )
   }
--- src/components/cart page/main/main.ts
+++ src/components/cart page/main/main.ts
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
 
 import { Button } from '../../checkout page/main/main'
+import {Div_flex} from '../../checkout page/main/main'
+import {Div_apply} from '../../modal box/modal'
+
 
 export const Main = styled.main`
     margin: 2% 1% 8% 1%;
 `
-
+export const Section_cart = styled.section`
+    display: flex;
+    justify-content: space-between;
+`
 export const Div_options = styled.div`
    display:flex;
    justify-content: space-between;
@@ -40,8 +46,61 @@ export const H1_main = styled.h1`
     }
 `
 
+export const Div_cart = styled.div`
+    width: 50%;
+`
+
+export const Div_name = styled.div`
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    margin-bottom: 1rem;
+
+    p{
+        font-weight: 500;
+        font-size: 1rem;
+        color: #626262;
+    }
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        width:40%
+    }
+`
+
+export const Div_product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const Div_product_descriptions = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 40%;
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        
+    }
+
+`
+
+export const Link_primary = styled.a`
+    color: #1B4B66;
+    font-weight: 600;
+    font-size: 0.875rem;
+`
+
+export const Link_Sencondary = styled(Link_primary)`
+    color: #B00020;
+`
+
 export const Div_order = styled.div`
-    width: 30%;
+    width: 33%;
 `
 
 export const P_order = styled.p`
@@ -53,6 +112,7 @@ export const P_order = styled.p`
 
 export const Button_order = styled(Button)`
     width: 11.25rem;
+    height: 2.5rem;
 `
 
 export const Button_continue = styled.button`
@@ -60,4 +120,15 @@ export const Button_continue = styled.button`
     border-radius: 8px;
     width: 11.25rem;
     height: 2.5rem;
+    color: #1B4B66;
+    font-weight: 500;
+    font-size: 16px;
+`
+
+export const Div_Flex_Cart = styled(Div_flex)`
+    border:none;
+`
+
+export const Div_apply_cart = styled(Div_apply)`
+    margin-top: 2rem;
 `

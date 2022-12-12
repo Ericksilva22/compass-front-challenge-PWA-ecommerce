fb5b6b1dbc8f2f76a02cb151cd5658232cf8a4f3
Erickson <ericksonrodrigues13@gmail.com>
2022-12-11 21:15:00 -0300

    Signup page: Add masked input

--- src/components/Signup/Phone/index.tsx
+++ src/components/Signup/Phone/index.tsx
@@ -1,14 +1,31 @@
-
-
+import { useState } from 'react'
 import {  Main, H1, Input, Input_DDNumber, P_signup, Button} from './phone'
+import React from 'react';
+import MaskedInput from 'react-text-mask';
 
 function SignupPhone() {
+  const [DD, setDD] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+
    return (
     <Main>
       <H1>Enter your phone number</H1>
       <div>
-        <Input_DDNumber placeholder='+55' type="text" name="" id="" />
-        <Input placeholder='(xx) x xxxx-xxxx' type="text" name="" id="" />
+      <MaskedInput
+          mask={['+', /[1-9]/, /\d/]}
+          placeholder='+55'
+          style={{backgroundColor: '#F1F1F1', border: 'none', borderRadius: '4px', width: '4.2rem', height: '2.625rem',paddingLeft: '1rem', marginRight: '1.4rem'}}
+          value={DD}
+          onChange = {(event: { target: { value: React.SetStateAction<string>; }; }) => setDD(event.target.value)} 
+        />
+
+        <MaskedInput
+          mask={['(', /[1-9]/, /\d/, ')', ' ', /\d/, ' ', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+          placeholder='(xx) x xxxx-xxxx'
+          style={{backgroundColor: '#F1F1F1', border: 'none', borderRadius: '4px', width: '17.98rem', height: '2.625rem',paddingLeft: '1rem'}}
+          value={phoneNumber}
+          onChange = {(event: { target: { value: React.SetStateAction<string>; }; }) => setphoneNumber(event.target.value)} 
+        />
       </div>
       <P_signup>A 4 digit code will be sent to this number.</P_signup>
       <Button>
--- src/components/Signup/Phone/phone.ts
+++ src/components/Signup/Phone/phone.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+
 export const Main = styled.main`
     
     @media (min-width: 426px) {
